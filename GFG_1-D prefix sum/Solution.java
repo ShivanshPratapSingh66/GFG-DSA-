4d2cf0e84c9873b732fd1fb02a6dfe5f59d0519e
Shivansh Pratap Singh <shivansh.pratap.singh8736@gmail.com>
2026-01-09 01:23:48 +0530

    Create Solution.java

---
+++ GFG_1-D prefix sum/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public ArrayList<Integer> prefSum(int[] arr) {
+        // code here
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        int sum=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            sum+=arr[i];
+            list.add(sum);
+        }
+        return list;
+    }
+}
